5d92ebf63c3d6d1b2579660d7f2e700555a4019f
seejl25 <seejunlong@u.nus.edu>
2025-02-27 23:31:40 +0800

    added in functions and events to make the calculator work

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ function divide(a,b) {
     return a/b;
 }
 
-let firstNum = [0,1,2,3,4,5,6,7,8,9];
-let secondNum = [0,1,2,3,4,5,6,7,8,9];
+let numList = [0,1,2,3,4,5,6,7,8,9];
 let operator = ["+", "-", "*", "/"];
 
 function operate(numOne, operation, numTwo) {
@@ -23,13 +22,49 @@ function operate(numOne, operation, numTwo) {
         return add(numOne, numTwo);
     }
     else if (operation === "-") {
-        return subtract(numOne. numTwo);
+        return subtract(numOne, numTwo);
     }
     else if (operation === "*") {
         return multiply(numOne, numTwo);
     }
     else if (operation === "/") {
+        if (numTwo === 0) {
+            return "MathError";
+        }
         return divide(numOne, numTwo);
     }
 }
 
+const btn = document.querySelectorAll("button");
+const display = document.querySelector(".text");
+const output = document.querySelector(".output");
+btn.forEach(eachBtn => {
+        eachBtn.addEventListener("click", () => {
+            if (eachBtn.textContent === "C") {
+                display.textContent = "";
+                output.textContent = "";
+            }
+            else if (eachBtn.textContent === "=") {
+                // if (display.textContent.length ===3) {
+                //     if (operator.includes(display.textContent[1]) && numList.includes(+display.textContent[0]) && numList.includes(+display.textContent[2])){
+                //         console.log("Yes");
+                //         output.textContent = operate(+display.textContent[0], display.textContent[1], +display.textContent[2]);
+                //     }
+                // }
+                const operand = operator.find(op => display.textContent.includes(op));
+                if (operand) {
+                    const idx = display.textContent.indexOf(operand);
+                    const firstNum = display.textContent.slice(0,idx);
+                    const secondNum = display.textContent.slice(idx+1);
+                    output.textContent = operate(+firstNum, operand, +secondNum);
+                }
+            }
+            else {
+                if (output.textContent !== "") {
+                    display.textContent = "";
+                    output.textContent = "";
+                }
+                display.textContent += eachBtn.textContent;
+            }
+}
+)});
