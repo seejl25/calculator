a1bf2e258fce81d10c8d211678064b4895dd6f91
seejl25 <seejunlong@u.nus.edu>
2025-02-28 23:24:14 +0800

    index.html changes: - added decimal and backspace button css changes: - color and font size - box-shadow - responsiveness javascript changes: - added new functions if operator button pressed - added functions for delete and decimal - added keyboard events for buttons

--- script.js
+++ script.js
@@ -40,23 +40,52 @@ const display = document.querySelector(".text");
 const output = document.querySelector(".output");
 btn.forEach(eachBtn => {
         eachBtn.addEventListener("click", () => {
-            if (eachBtn.textContent === "C") {
+            if (eachBtn.textContent === "AC") {
                 display.textContent = "";
                 output.textContent = "";
             }
+            else if (eachBtn.textContent === "Del") {
+                display.textContent = display.textContent.replace(display.textContent.at(-1), '');
+            }
+            else if (eachBtn.textContent === ".") {
+                if (display.textContent.includes(".")) {
+                    display.textContent += "";
+                }
+                else {
+                    display.textContent += eachBtn.textContent;
+                }
+            }
             else if (eachBtn.textContent === "=") {
-                // if (display.textContent.length ===3) {
-                //     if (operator.includes(display.textContent[1]) && numList.includes(+display.textContent[0]) && numList.includes(+display.textContent[2])){
-                //         console.log("Yes");
-                //         output.textContent = operate(+display.textContent[0], display.textContent[1], +display.textContent[2]);
-                //     }
-                // }
                 const operand = operator.find(op => display.textContent.includes(op));
                 if (operand) {
                     const idx = display.textContent.indexOf(operand);
                     const firstNum = display.textContent.slice(0,idx);
                     const secondNum = display.textContent.slice(idx+1);
-                    output.textContent = operate(+firstNum, operand, +secondNum);
+                    if (!secondNum) {
+                        output.textContent = "ERROR";
+                    }
+                    else {
+                        output.textContent = operate(+firstNum, operand, +secondNum)
+                    };
+                }
+                else {
+                    output.textContent = display.textContent;
+                }
+            }
+            else if (operator.includes(eachBtn.textContent)) {
+                const operand = operator.find(op => display.textContent.includes(op));
+                if (operand && output.textContent === "") {
+                    const idx = display.textContent.indexOf(operand);
+                    const firstNum = display.textContent.slice(0,idx);
+                    const secondNum = display.textContent.slice(idx+1);
+                    display.textContent = operate(+firstNum, operand, +secondNum) + eachBtn.textContent;
+                }
+                else if (output.textContent !== "") {
+                    display.textContent = output.textContent + eachBtn.textContent;
+                    output.textContent = "";
+                }
+                else {
+                    display.textContent += eachBtn.textContent;
                 }
             }
             else {
@@ -68,3 +97,62 @@ btn.forEach(eachBtn => {
             }
 }
 )});
+
+window.onkeydown = function(event) {
+    if (event.keyCode === 46) {
+        display.textContent = "";
+        output.textContent = "";
+    }
+    else if (event.keyCode === 8) {
+        display.textContent = display.textContent.replace(display.textContent.at(-1), '');
+    }
+    else if (event.keyCode === 190) {
+        if (display.textContent.includes(".")) {
+            display.textContent += "";
+        }
+        else {
+            display.textContent += ".";
+        }
+    }
+    else if (event.keyCode === 187 || event.keyCode === 13) {
+        const operand = operator.find(op => display.textContent.includes(op));
+        if (operand) {
+            const idx = display.textContent.indexOf(operand);
+            const firstNum = display.textContent.slice(0,idx);
+            const secondNum = display.textContent.slice(idx+1);
+            if (!secondNum) {
+                output.textContent = "ERROR";
+            }
+            else {
+                output.textContent = operate(+firstNum, operand, +secondNum)
+            };
+        }
+        else {
+            output.textContent = display.textContent;
+        }
+    }
+    else if (event.keyCode === 107 || event.keyCode === 109 || event.keyCode === 106 || event.keyCode === 111) {
+        const operand = operator.find(op => display.textContent.includes(op));
+        if (operand && output.textContent === "") {
+            const idx = display.textContent.indexOf(operand);
+            const firstNum = display.textContent.slice(0,idx);
+            const secondNum = display.textContent.slice(idx+1);
+            display.textContent = operate(+firstNum, operand, +secondNum) + event.key;
+        }
+        else if (output.textContent !== "") {
+            display.textContent = output.textContent + event.key;
+            output.textContent = "";
+        }
+        else {
+            display.textContent += event.key;
+        }
+    }
+    else if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
+        if (output.textContent !== "") {
+            display.textContent = "";
+            output.textContent = "";
+        }
+        display.textContent += event.key;
+    }
+}
+
